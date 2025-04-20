6def184693c85342eb19da0c120ad1a261d8a325
dcorks <dcorks@gmail.com>
2025-04-20 19:57:40 +0900

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -2,9 +2,9 @@
 
 # -- Project information
 
-project = 'Lumache'
-copyright = '2021, Graziella'
-author = 'Graziella'
+project = 'Digital Literacy Curriculum'
+copyright = '2025, Daniel Corks'
+author = 'Daniel Corks'
 
 release = '0.1'
 version = '0.1.0'
